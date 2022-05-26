cbc0c07edbf03ae80dd4e4e04ddce30953ed7307
R.Noto <rikitonoto@gmail.com>
2022-05-26 23:40:49 +0900

    partial schedule and scheduler

--- src/Scheduler/src/Scheduler.h
+++ src/Scheduler/src/Scheduler.h
@@ -2,24 +2,7 @@
 #define _SCHEDULER_H_
 
 #include "DataType.h"
-
-/**
-* @class Schedule_if
-* @brief interface of scheduler functions.
-*/
-class Schedule_if
-{
-public:
-    virtual ~Schedule_if(){};
-    virtual void execute(void) = 0;
-    virtual DWORD getTime(void) = 0;
-    enum SchduleType : BYTE
-    {
-        INTERVAL,       //  execute intarval for the time.
-        AFTER_ONCE      //  execute once after the time.
-    };
-    virtual SchduleType getScheduleType(void) = 0;
-};
+#include "Schedule_if.h"
 
 /**
 * @class Scheduler
